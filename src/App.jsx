93cf7750acef11351fee547bbf152c165f245fad
Krisanapong Jaruenjanporn <150851052+Krianapong@users.noreply.github.com>
2024-03-11 17:01:39 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import Footer from "./components/footer";
+import Header from "./components/header";
+import SearchQuery from "./components/search_query";
 function App() {
   return (
     <div>
+      <Header />
+
+      <div className="container mx-auto">
+        <SearchQuery />
+      </div>
       <Footer />
     </div>
   );
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,23 +1,23 @@
-    // Footer.js
-    import React from "react";
 
-    const Footer = () => {
-    return (
-        <footer className="bg-gray-800 text-gray-300 py-4">
-        <div className="container mx-auto flex flex-col lg:flex-row items-center justify-end">
-            <div className="text-right lg:order-2">
-            <img
-                className="max-h-12 w-auto object-contain mb-2 lg:order-1"
-                src="https://tailwindui.com/img/logos/158x48/reform-logo-gray-900.svg"
-                alt="Reform"
-                width={79}
-                height={24}
-            />
-            <p className="text-sm">&copy; ME Group Enterprise Co,. Ltd.</p>
-            </div>
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-gray-300 py-2">
+      <div className="container flex flex-col lg:flex-row items-center justify-end">
+        <div>
+          <img
+            className="max-h-12 w-auto object-contain "
+            src="https://tailwindui.com/img/logos/158x48/reform-logo-gray-900.svg"
+            alt="Reform"
+            width={79}
+            height={24}
+          />
+          <p className="text-xs">&copy; ME Group Enterprise Co,. Ltd.</p>
         </div>
-        </footer>
-    );
-    };
+      </div>
+    </footer>
+  );
+};
 
-    export default Footer;
+export default Footer;
